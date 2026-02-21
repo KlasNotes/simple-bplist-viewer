bd7c7a2960ff87dc92311e1a9911f10ede5c9fa6
KlasNotes <37313304+KlasNotes@users.noreply.github.com>
2026-02-21 13:49:35 +0000

    Update setup.py for build options and MSI config

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
 import sys
+import uuid
 from cx_Freeze import setup, Executable
 
-# 1. Trimming the fat: List modules we definitely DON'T need
+# 1. Permanent Upgrade Code (Do not change this once set)
+# This allows Windows to recognize updates to the same app.
+UPGRADE_CODE = "{C6B1B823-A23E-4D6B-8833-D659E79178C4}"
+
+# 2. Refined Build Options
 build_exe_options = {
-    "packages": ["os"],
+    "packages": ["os", "PySide6.QtCore", "PySide6.QtGui", "PySide6.QtWidgets"],
     "excludes": [
-        "tkinter",
-        "unittest",
-        "email",
-        "http",
-        "xml",
-        "pydoc",
-        "PySide6.QtWebEngine",
-        "PySide6.QtWebEngineCore",
-        "PySide6.QtQuick",
-        "PySide6.QtNetwork",
-        "PySide6.QtPdf",
-        "PySide6.QtMultimedia",
-        "PySide6.QtVirtualKeyboard",
-        "PySide6.QtDesigner",
+        "tkinter", "unittest", "email", "http", "xml", "pydoc",
+        "PySide6.QtWebEngine", "PySide6.QtWebEngineCore", "PySide6.QtQuick",
+        "PySide6.QtNetwork", "PySide6.QtPdf", "PySide6.QtMultimedia",
+        "PySide6.QtVirtualKeyboard", "PySide6.QtDesigner",
     ],
-    # 2. This zips the libraries into one file, saving a huge amount of space
-    "zip_include_packages": ["PySide6", "shiboken6"],
+    # NOTE: We keep PySide6 out of the zip to prevent "Plugin not found" errors
+    "zip_include_packages": ["shiboken6"],
+    "include_msi": True,
+}
+
+# 3. MSI Specific Configuration
+bdist_msi_options = {
+    "upgrade_code": UPGRADE_CODE,
+    "add_to_path": False,
+    "initial_target_dir": r"[ProgramFilesFolder]\SimpleBPlistViewer",
+    "install_icon": None, # Add a path to an .ico file here if you have one
 }
 
 base = None
@@ -31,13 +35,17 @@ if sys.platform == "win32":
 setup(
     name="Simple bPlist Viewer",
     version="0.1",
+    author="Your Name",
     description="Binary Plist Viewer for Forensics",
-    options={"build_exe": build_exe_options},
+    options={
+        "build_exe": build_exe_options,
+        "bdist_msi": bdist_msi_options,
+    },
     executables=[
         Executable(
             "binary_plist.py", 
             base=base,
-            target_name="bplist-viewer.exe", # Cleaner name
+            target_name="bplist-viewer.exe",
             shortcut_name="bPlist Viewer",
             shortcut_dir="DesktopFolder"
         )
